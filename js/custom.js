a6ec5ab7c5a195ab13609a5fe89a00a42e28fd19
jkhkhkahjkfd@gmail <jkhkhkahjkfd@gmail.com>
2024-10-16 17:02:53 +0700

    1

--- js/custom.js
+++ js/custom.js
@@ -20,27 +20,3 @@ function search_animal() {
     }
   }
 }
-
-
-// share button script
-
-function updateModal() {
-  var currentUrl = window.location.href;
-  document.getElementById("urlInput").value = currentUrl;
-}
-
-function copyToClipboard() {
-  var copyText = document.getElementById("urlInput");
-  copyText.select();
-  document.execCommand("copy");
-  document.getElementById("copyButton").innerHTML = "Copied";
-}
-
-document.getElementById("shareButton").addEventListener("click", function () {
-  updateModal();
-  $('#shareModal').modal('show');
-});
-
-document.getElementById("copyButton").addEventListener("click", function () {
-  copyToClipboard();
-});
